5a4c587d881233d279a095056f8089254cec02b3
Orfey-creator <57431002+Orfey-creator@users.noreply.github.com>
2020-09-24 13:54:25 +0300

    create: tabs

--- js/main.js
+++ js/main.js
@@ -130,3 +130,40 @@ const ban = () => {
 	});
 };
 ban();
+
+const tabs = () => {
+	const tabHeader = document.querySelectorAll(".panel-group"),
+		tab = document.querySelectorAll("[role=tab]"),
+		tabContent = document.querySelectorAll("[role=tabpanel]");
+	const toggleTabContent = (index) => {
+		for (let i = 0; i < tabContent.length; i++) {
+			if (index === i) {
+				tab[i].style.display = 'block';
+				tabContent[i].style.display = 'block';
+			} else {
+				tab[i].classList.remove("active");
+				tabContent[i].style.display = 'none';
+			}
+		}
+	};
+	tabHeader.forEach((elem) => {
+		elem.addEventListener("click", (event) => {
+			event.preventDefault();
+			let target = event.target;
+			target = target.closest(".panel-heading");
+			if (target) {
+				tab.forEach((item, i) => {
+					if (item === target) {
+						toggleTabContent(i);
+					}
+				});
+			}
+		});
+	});
+};
+tabs();
+
+const calc = () => {
+
+};
+calc();
